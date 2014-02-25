a0c527952bbea3f03ae7ffb55bfb161c930e5358
Howard King <rjxx007@sina.com>
2014-02-25 10:06:04 +0800

    Add the test files of Makefile

---
+++ Makefile/make2/src/compare.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+int main(void)
+{
+	char arr[20] = "hello kitty"; 
+	char arr2[20] = "hello world";
+
+	printf("%d\n", strcmp(arr,arr2));
+
+	return 0;
+}
---
+++ helloworld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("This is not 'Hello World'\n");
+
+	return 0;
+}
+
+------------------------------------------------
+console:
+This is not 'Hello World'
